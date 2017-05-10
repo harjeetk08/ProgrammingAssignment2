ccb08ce1bf91541625878b9e8866952a17234c6f
harjeetk08 <Harjeet.Kaur@kp.org>
2017-05-10 09:27:14 -0700

    Cachematrix
    solution for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ##This function creates a special "matrix object thatcan cache its inverse.
 
-'makeCacheMatrix <- function(x = matrix()) {
+   makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
   	set <- function(y){
    	x <<- y
@@ -14,14 +14,14 @@
         getInverse <- function() inv
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
-}'
+   }
 
 
 
 ## This function computes the inverse of the special "matrix" returned by 
     `makeCacheMatrix` above. 
 
-'cacheSolve <- function(x, ...) {
+  cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
           inv <- x$getInverse()
           if(!is.null(inv)){
@@ -32,4 +32,4 @@
           inv <- solve(data)
           x$setInverse(inv)
           inv  
-}'
+   }
